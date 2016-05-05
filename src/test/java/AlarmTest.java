50f6ca0c8fbe5050bc3c7e624f507889f221e8e9
trikitrok <trikitrok@gmail.com>
2016-05-05 12:33:46 +0200

    testing that alarm is off when probed pressure is inside safety range

--- src/test/java/AlarmTest.java
+++ src/test/java/AlarmTest.java
@@ -7,17 +7,32 @@ import static org.hamcrest.core.Is.is;
 public class AlarmTest {
     @Test
     public void alarm_is_on_when_pressure_is_too_low() {
-        Alarm alarm = new FakeAlarm();
+        Alarm alarm = new FakeAlarm(5.0);
 
         alarm.check();
 
         assertThat(alarm.isAlarmOn(), is(true));
     }
 
+    @Test
+    public void alarm_is_off_when_pressure_is_inside_safety_range() {
+        Alarm alarm = new FakeAlarm(18.0);
+
+        alarm.check();
+
+        assertThat(alarm.isAlarmOn(), is(false));
+    }
+
     class FakeAlarm extends Alarm {
+        private final double probedValue;
+
+        public FakeAlarm(double probedValue) {
+            this.probedValue = probedValue;
+        }
+
         @Override
         protected double probePressureValue() {
-            return 5.0;
+            return probedValue;
         }
     }
 }
