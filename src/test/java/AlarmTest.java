8971290596c5bd758be901cbef6afa4e4881e474
trikitrok <trikitrok@gmail.com>
2016-05-05 13:01:07 +0200

    first test using new constructor

--- src/test/java/AlarmTest.java
+++ src/test/java/AlarmTest.java
@@ -4,13 +4,16 @@ import tddmicroexercises.tirepressuremonitoringsystem.Sensor;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 public class AlarmTest {
     @Test
     public void alarm_is_on_when_pressure_is_too_low() {
-        Alarm alarm = new FakeAlarm(5.0);
+        Sensor sensor = mock(Sensor.class);
+        doReturn(5.0).when(sensor).popNextPressurePsiValue();
+        Alarm alarm = new Alarm(sensor);
 
         alarm.check();
 
